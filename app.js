33164c2f717bf17c5938654c2bdf1f7000d588d6
VarenytsiaV <victoriavarenytsia@gmail.com>
2024-11-26 18:30:19 +0200

    validation added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
 
 const app = express()
+
 app.use(express.static('public'))
+app.use(express.json())
 app.set('view engine', 'ejs')
 
 const dbURI =
@@ -16,3 +19,4 @@ mongoose
 
 app.get('/', (req, res) => res.render('home'))
 app.get('/smoothies', (req, res) => res.render('smoothies'))
+app.use(authRoutes)
